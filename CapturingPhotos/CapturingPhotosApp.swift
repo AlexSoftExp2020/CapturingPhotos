7c698c6fd9623831adbc4a491a1fb410789dd7f1
Alex <oleksii.pidhornyi@gmail.com>
2025-02-12 21:33:21 +0100

    Initial Commit

---
+++ CapturingPhotos/CapturingPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapturingPhotosApp.swift
+//  CapturingPhotos
+//
+//  Created by Oleksii on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CapturingPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
