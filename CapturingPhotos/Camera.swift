6303ba93d561e4de66a847128ea9e593860b91fc
Alex <oleksii.pidhornyi@gmail.com>
2025-02-13 15:37:31 +0100

    add update video output connection

--- CapturingPhotos/Camera.swift
+++ CapturingPhotos/Camera.swift
@@ -233,4 +233,12 @@ class Camera: NSObject {
         }
         updateVideoOutputConnection()
     }
+    
+    private func updateVideoOutputConnection() {
+        if let videoOutput = videoOutput, let videoOutputConnection = videoOutput.connection(with: .video) {
+            if videoOutputConnection.isVideoMirroringSupported {
+                videoOutputConnection.isVideoMirrored = isUsingFrontCaptureDevice
+            }
+        }
+    }
 }
