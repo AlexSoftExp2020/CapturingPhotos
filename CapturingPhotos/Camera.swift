d69b0751a2660882b97f8380e25e600ea36575d5
Alex <oleksii.pidhornyi@gmail.com>
2025-02-13 12:55:23 +0100

    add camera

--- CapturingPhotos/Camera.swift
+++ CapturingPhotos/Camera.swift
@@ -5,4 +5,54 @@
 //  Created by Oleksii on 13.02.2025.
 //
 
-import Foundation
+import AVFoundation
+import CoreImage
+import UIKit
+import os.log
+
+class Camera: NSObject {
+    private let captureSession = AVCaptureSession()
+    private var isCaptureSessionConfigured = false
+    private var deviceInput: AVCaptureDeviceInput?
+    private var photoOutput: AVCapturePhotoOutput?
+    private var videoOutput: AVCaptureVideoDataOutput?
+    private var sessionQueue: DispatchQueue!
+    
+    private var allCaptureDevices: [AVCaptureDevice] {
+        AVCaptureDevice.DiscoverySession(deviceTypes:
+                                            [
+                                                .builtInTrueDepthCamera,
+                                                .builtInDualCamera,
+                                                .builtInDualWideCamera,
+                                                .builtInWideAngleCamera,
+                                                .builtInDualWideCamera
+                                            ],
+                                         mediaType: .video,
+                                         position: .unspecified).devices
+    }
+    
+    private var frontCaptureDevices: [AVCaptureDevice] {
+        allCaptureDevices
+            .filter { $0.position == .front }
+    }
+    
+    private var backCaptureDevices: [AVCaptureDevice] {
+        allCaptureDevices
+            .filter { $0.position == .back }
+    }
+    
+    private var captureDevices: [AVCaptureDevice] {
+        var devices = [AVCaptureDevice]()
+#if os(macOS) || (os(iOS) && targetEnvironment(macCatalyst))
+        devices += allCaptureDevices
+#else
+        if let backDevice = backCaptureDevices.first {
+            devices += [backDevice]
+        }
+        if let frontDevice = frontCaptureDevices.first {
+            devices += [frontDevice]
+        }
+#endif
+        return devices
+    }
+}
