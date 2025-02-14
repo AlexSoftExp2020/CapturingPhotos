6fe5a63cae10dd53662c7d69f714d94e5db12f24
Alex <oleksii.pidhornyi@gmail.com>
2025-02-14 21:43:26 +0100

    add buttons views to camera view

--- CapturingPhotos/CameraView.swift
+++ CapturingPhotos/CameraView.swift
@@ -47,6 +47,60 @@ struct CameraView: View {
             .statusBar(hidden: true)
         }
     }
+    
+    private func buttonsView() -> some View {
+        HStack(spacing: 60) {
+            
+            Spacer()
+            
+            NavigationLink {
+                PhotoCollectionView(photoCollection: model.photoCollection)
+                    .onAppear {
+                        model.camera.isPreviewPaused = true
+                    }
+                    .onDisappear {
+                        model.camera.isPreviewPaused = false
+                    }
+            } label: {
+                Label {
+                    Text("Gallery")
+                } icon: {
+                    ThumbnailView(image: model.thumbnailImage)
+                }
+            }
+            
+            Button {
+                model.camera.takePhoto()
+            } label: {
+                Label {
+                    Text("Take Photo")
+                } icon: {
+                    ZStack {
+                        Circle()
+                            .strokeBorder(.white, lineWidth: 3)
+                            .frame(width: 62, height: 62)
+                        Circle()
+                            .fill(.white)
+                            .frame(width: 50, height: 50)
+                    }
+                }
+            }
+            
+            Button {
+                model.camera.switchCaptureDevice()
+            } label: {
+                Label("Switch Camera", systemImage: "arrow.triangle.2.circlepath")
+                    .font(.system(size: 36, weight: .bold))
+                    .foregroundColor(.white)
+            }
+            
+            Spacer()
+        
+        }
+        .buttonStyle(.plain)
+        .labelStyle(.iconOnly)
+        .padding()
+    }
 }
 
 #Preview {
