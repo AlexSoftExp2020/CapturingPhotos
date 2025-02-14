171edf076c15ead96fc07ad3d9c5c39824813128
Alex <oleksii.pidhornyi@gmail.com>
2025-02-14 21:39:39 +0100

    add properties to camera view

--- CapturingPhotos/CameraView.swift
+++ CapturingPhotos/CameraView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct CameraView: View {
+    @StateObject private var model = DataModel()
+    
+    private static let barHeightFactor = 0.15
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
