7e4daf3596aaefd8b9538c85260a2c5b7f9d5bf8
Alex <oleksii.pidhornyi@gmail.com>
2025-02-14 21:40:41 +0100

    add body to camera view

--- CapturingPhotos/CameraView.swift
+++ CapturingPhotos/CameraView.swift
@@ -13,7 +13,39 @@ struct CameraView: View {
     private static let barHeightFactor = 0.15
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            GeometryReader { geometry in
+                ViewfinderView(image:  $model.viewfinderImage )
+                    .overlay(alignment: .top) {
+                        Color.black
+                            .opacity(0.75)
+                            .frame(height: geometry.size.height * Self.barHeightFactor)
+                    }
+                    .overlay(alignment: .bottom) {
+                        buttonsView()
+                            .frame(height: geometry.size.height * Self.barHeightFactor)
+                            .background(.black.opacity(0.75))
+                    }
+                    .overlay(alignment: .center)  {
+                        Color.clear
+                            .frame(height: geometry.size.height * (1 - (Self.barHeightFactor * 2)))
+                            .accessibilityElement()
+                            .accessibilityLabel("View Finder")
+                            .accessibilityAddTraits([.isImage])
+                    }
+                    .background(.black)
+            }
+            .task {
+                await model.camera.start()
+                await model.loadPhotos()
+                await model.loadThumbnail()
+            }
+            .navigationTitle("Camera")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
+            .ignoresSafeArea()
+            .statusBar(hidden: true)
+        }
     }
 }
 
