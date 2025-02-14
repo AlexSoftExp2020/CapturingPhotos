622bfa0058a514b03e4b96fb319079b993d84354
Alex <oleksii.pidhornyi@gmail.com>
2025-02-14 20:45:02 +0100

    add update for device orientation

--- CapturingPhotos/Camera.swift
+++ CapturingPhotos/Camera.swift
@@ -293,5 +293,8 @@ class Camera: NSObject {
         return orientation
     }
     
-    
+    @objc
+    func updateForDeviceOrientation() {
+        //TODO: Figure out if we need this for anything.
+    }
 }
