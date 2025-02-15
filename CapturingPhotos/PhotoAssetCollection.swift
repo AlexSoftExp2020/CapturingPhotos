de6529256f708bc55642b0a2eac664d8dde0e38e
Alex <oleksii.pidhornyi@gmail.com>
2025-02-15 10:39:22 +0100

    add phot asset collection

---
+++ CapturingPhotos/PhotoAssetCollection.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoAssetCollection.swift
+//  CapturingPhotos
+//
+//  Created by Oleksii on 14.02.2025.
+//
+
+import Photos
+
+class PhotoAssetCollection: RandomAccessCollection {
+    private(set) var fetchResult: PHFetchResult<PHAsset>
+    private var iteratorIndex: Int = 0
+    
+    private var cache = [Int : PhotoAsset]()
+    
+    var startIndex: Int { 0 }
+    var endIndex: Int { fetchResult.count }
+    
+    
+    
+}
