e4a0ad8b10edea17da27a92b0cabc0df7b073823
Alex <oleksii.pidhornyi@gmail.com>
2025-02-15 16:55:20 +0100

    add photo view

---
+++ CapturingPhotos/PhotoView.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoView.swift
+//  CapturingPhotos
+//
+//  Created by Oleksii on 15.02.2025.
+//
+
+import SwiftUI
+import Photos
+
+struct PhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoView()
+}
