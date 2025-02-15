0f047ed0dfd1e8472a3f62862e6f8b44b113f771
Alex <oleksii.pidhornyi@gmail.com>
2025-02-15 16:25:19 +0100

    add photo collection view

---
+++ CapturingPhotos/PhotoCollectionView.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCollectionView.swift
+//  CapturingPhotos
+//
+//  Created by Oleksii on 15.02.2025.
+//
+
+import SwiftUI
+import os.log
+
+struct PhotoCollectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoCollectionView()
+}
