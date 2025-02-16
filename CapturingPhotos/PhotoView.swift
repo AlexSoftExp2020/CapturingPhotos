9a94f7b94b9b06e00b5d335a351867696fd12d50
Alex <oleksii.pidhornyi@gmail.com>
2025-02-16 19:14:11 +0100

    add properties to photo view

--- CapturingPhotos/PhotoView.swift
+++ CapturingPhotos/PhotoView.swift
@@ -9,6 +9,13 @@ import SwiftUI
 import Photos
 
 struct PhotoView: View {
+    var asset: PhotoAsset
+    var cache: CachedImageManager?
+    @State private var image: Image?
+    @State private var imageRequest: PHImageRequestID?
+    @Environment(\.dismiss) var dismiss
+    private let imageSize = CGSize(width: 1024, height: 1024)
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
