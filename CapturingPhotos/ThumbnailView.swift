e8e8b8598045bc18bd3a744688be9e88dea27123
Alex <oleksii.pidhornyi@gmail.com>
2025-02-16 19:19:52 +0100

    add thumbnail view

---
+++ CapturingPhotos/ThumbnailView.swift
@@ -0,0 +1,18 @@
+//
+//  ThumbnailView.swift
+//  CapturingPhotos
+//
+//  Created by Oleksii on 16.02.2025.
+//
+
+import SwiftUI
+
+struct ThumbnailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThumbnailView()
+}
