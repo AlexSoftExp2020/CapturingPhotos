0ae5472bf65bc03f677957a3080deb7faf3e13d4
Alex <oleksii.pidhornyi@gmail.com>
2025-02-16 19:27:55 +0100

    add viewfinder view

---
+++ CapturingPhotos/ViewfinderView.swift
@@ -0,0 +1,18 @@
+//
+//  ViewfinderView.swift
+//  CapturingPhotos
+//
+//  Created by Oleksii on 16.02.2025.
+//
+
+import SwiftUI
+
+struct ViewfinderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewfinderView()
+}
